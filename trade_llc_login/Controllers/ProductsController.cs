94aaa1d065df7b0b134692813a5c1a061639cebf
Mitchell Sotto <sottochoro@gmail.com>
2017-11-13 13:36:50 -0700

    added ugly drowdown to Products page

---
+++ trade_llc_login/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace trade_llc_login.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            List<SelectListItem> listItems = new List<SelectListItem>();
+            listItems.Add(new SelectListItem
+            {
+                Text = "Beans",
+                Value = "Beans"
+            });
+            listItems.Add(new SelectListItem
+            {
+                Text = "Nuts",
+                Value = "Nuts"
+            });
+            listItems.Add(new SelectListItem
+            {
+                Text = "Dried Fruits",
+                Value = "Dried Fruits"
+            });
+
+            ViewBag.listItems = listItems;
+            return View();
+        }
+    }
+}
