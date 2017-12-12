11fca8411fc4f4a6368d4ffdbe92a1fd51c1a1ed
Leo Liang <leoliang@byu.edu>
2017-12-12 14:25:09 -0700

    add users model

---
+++ trade_llc_login/Models/Users.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace trade_llc_login.Models
+{
+    [Table("Users")]
+    public class Users
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage = "Please enter your first name")]
+        [DisplayName("First Name")]
+        [StringLength(255)]
+        public string UserFirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your last name")]
+        [DisplayName("Last Name")]
+        [StringLength(255)]
+        public string UserLastName { get; set; }
+
+        [Required(ErrorMessage = "The Email is required")]
+        [EmailAddress]
+        [DisplayName("Email")]
+        public string UserEmail { get; set; }
+
+        [Required(ErrorMessage = "Password is requires")]
+        [DisplayName("Password")]
+        [StringLength(255)]
+        public string UserPassword { get; set; }
+
+
+    }
+}
